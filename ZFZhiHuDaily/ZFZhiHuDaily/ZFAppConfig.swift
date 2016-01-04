90e5107a79d43791a4917af4b91ea6e5e7d8888a
zifeng <459643690@qq.com>
2016-01-04 22:32:26 +0800

    mark

--- ZFZhiHuDaily/ZFZhiHuDaily/ZFAppConfig.swift
+++ ZFZhiHuDaily/ZFZhiHuDaily/ZFAppConfig.swift
@@ -15,3 +15,4 @@ let ScreenHeight : CGFloat = UIScreen.mainScreen().bounds.height
 func RGBA (r:CGFloat,_ g:CGFloat, _ b:CGFloat,_ a:CGFloat) -> UIColor {
     return UIColor(red: r/255.0, green: g/255.0, blue: b/255.0, alpha: a)
 }
+// MARK: - 呵呵
