fe21610616c4e15fa656970f98a5a41cb39bf3ee
renzifeng <459643690@qq.com>
2016-01-29 22:55:02 +0800

    update

---
+++ ZFZhiHuDaily/NewsComment/ViewModel/ZFNewsCommentViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  ZFNewsCommentViewModel.swift
+//  ZFZhiHuDaily
+//
+//  Created by 任子丰 on 16/1/29.
+//  Copyright © 2016年 任子丰. All rights reserved.
+//
+
+import UIKit
+
+class ZFNewsCommentViewModel {
+    typealias LongCommentSuccessBlock = (newsId: String) -> Void
+    
+//    func getLongComment(#long)
+}
